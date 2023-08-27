2628b997211cc70716591858c9e808af64eb276a
GergelyTuri <gt2253@columbia.edu>
2023-08-27 15:02:20 -0400

    refactoring, adding new video class

---
+++ src/video.py
@@ -0,0 +1,36 @@
+"""
+Routine to handle video files.
+by: Gergely Turi
+gt2253@cumc.columbia.edu 
+"""
+from datetime import datetime
+
+import tdt
+from dataclasses import dataclass
+
+@dataclass
+class Video:
+    """Video class to handle video files recored with USB camera. """
+    def __init__(self, f_path):
+        self.f_path = f_path
+        self.data = tdt.read_block(self.f_path)
+
+    def load_video_meta_data(self):
+        """Loads video file and returns the data as numpy array."""
+        return self.data['epocs']['Cam1']
+
+    def recording_length(self):
+        """Calculates frame rate based on the video meta data."""
+        start = datetime.strptime(self.data['info']['start'],
+                                   '%I:%M:%S%p %m/%d/%Y') 
+        stop = datetime.strptime(self.data['info']['stop'],
+                                  '%I:%M:%S%p %m/%d/%Y') 
+        recording_lenght = stop - start
+        return recording_lenght
+    
+    def frame_rate(self):
+        """Calculates frame rate based on the video meta data."""
+        recording_lenght = self.recording_length()
+        frames = len(self.data['epochs']['Cam1']['data'])
+        frame_rate = frames/recording_lenght
+        return frame_rate
