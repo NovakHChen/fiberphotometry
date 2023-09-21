9b569ff8d1dc9117779bd87955bc6149c3f3e4ea
gergely <gt2253@cumc.columbia.edu>
2023-09-21 10:18:49 -0400

    slight mod in video.py

--- src/video.py
+++ src/video.py
@@ -11,13 +11,21 @@ import tdt
 class Video:
     """Video class to handle video files recored with USB camera."""
 
-    def __init__(self, f_path):
-        self.f_path = f_path
+    f_path: str
+    
+    def __post_init__(self):
+        """Reads in the data from the hdf5 file."""
         self.data = tdt.read_block(self.f_path)
 
-    def frame_rate(self):
-        """Calculates frame rate based on the video meta data."""
+    def frame_rate(self, rounded=True) -> float or int:
+        """Calculates frame rate based on the video meta data.
+        Returns: int or float depending on the rounded parameter.
+        """
         recording_lenght = self.data.info["duration"].total_seconds()
         frames = len(self.data["epocs"]["Cam1"]["data"])
-        fr_rate = frames / recording_lenght
+        f_rate = frames / recording_lenght
+        if rounded:
+            fr_rate = round(f_rate)
+        else:
+            fr_rate = f_rate
         return fr_rate
