3d87322cc6b191effe96aaf57430fc1e56c2bee9
gergely <gt2253@cumc.columbia.edu>
2023-09-25 18:21:57 -0400

    working on fiberohotometry and video modules

--- src/fiberphotometry.py
+++ src/fiberphotometry.py
@@ -1,171 +1,198 @@
 # scripts for fiber photometry recordings. 
 # GT 2021
 
+from dataclasses import dataclass
+from tdt import read_block
+
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import zscore
 from scipy.signal import decimate
 import pandas as pd
-from tdt import read_block, read_sev, epoc_filter 
+
 matplotlib.rcParams['font.size'] = 18 # set font size for all figures
 
 # from bokeh.plotting import figure, show
 # from bokeh.io import push_notebook, output_notebook
 # output_notebook()
 
-# global variables
-GCAMP = '_465A' # GCaMP channel (dynamic signal) 
-ISOS = '_405A' # Isobestic channel (static signal)
 
-def resample(data):
-    """making a time array based on the number of samples 
-    and the sample frequency sampling
-    
-    Parameters:
-    -----------
-    data : tdt block"""
-    
-    npts = len(data.streams[GCAMP].data)
-    time_x = np.linspace(1, npts, npts) / data.streams[GCAMP].fs
-    
-    return time_x
+@dataclass
+class FiberPhotometry:
+    """Class for analyzing fiber photometry data."""
 
-def downsampling(data, N=10):
-    """downsampling the data 
+    tank_path: str
+    # global variables
+    GCAMP = '_465A' # GCaMP channel (dynamic signal) 
+    ISOS = '_405A' # Isobestic channel (static signal)
     
-    Parameters:
-    -----------
-    data : tdt block
-    channel : global variable
-        either GCAMP or ISOS
-    N : int
-        number of points for averaging
+    def __post_init__(self):
+        photometry_data = read_block(self.tank_path)
+    
+    def resample(self):
+        """making a time array based on the number of samples 
+        and the sample frequency sampling
         
-    Returns:
-    --------
-    dictionary of numpy arrays with the decimated signal and time 
-    """
-    
-    decimated_GCAMP = []
-    decimated_ISOS = []
+        Parameters:
+        -----------
+        photometry_data : tdt block"""
+        
+        npts = len(self.photometry_data.streams[GCAMP].photometry_data)
+        time_x = np.linspace(1, npts, npts) / photometry_data.streams[GCAMP].fs
+        
+        return time_x
+
+    def downsampling(self, photometry_data, N=10):
+        """downsampling the photometry_data 
+        
+        Parameters:
+        -----------
+        photometry_data : tdt block
+        channel : global variable
+            either GCAMP or ISOS
+        N : int
+            number of points for averaging
             
-    for i in range(0, len(data.streams[GCAMP].data), N):        
-        # This is the moving window mean
-        decimated_GCAMP.append(np.mean(np.asarray(data.streams[GCAMP].data[i:i+N-1])))
-    data.streams[GCAMP].data = np.asarray(decimated_GCAMP)
-    
-    for i in range(0, len(data.streams[ISOS].data), N):        
-        # This is the moving window mean
-        decimated_ISOS.append(np.mean(np.asarray(data.streams[ISOS].data[i:i+N-1])))
-    data.streams[ISOS].data = np.asarray(decimated_ISOS)
-
-    time_x = np.linspace(1,len(data.streams[GCAMP].data),
-     len(data.streams[GCAMP].data))/data.streams[GCAMP].fs
-    # time_x = resample(data)
-    # time_x = time_x[::N] # go from beginning to end of array in steps on N
-    # time_x = time_x[:len(data.streams[GCAMP].data)]
+        Returns:
+        --------
+        dictionary of numpy arrays with the decimated signal and time 
+        """
         
-    return data
-
-def artifact_removal(data):
-    """There is often a large artifact on the onset
-       of LEDs turning on. This function removes data below a set time t
-       
-    Parameters:
-    -----------
-    data : tdt block
-    channel : global variable
-        either GCAMP or ISOS
-     """
-    
-    
-    t = 8
-    time_x = resample(data)
-    
-    inds = np.where(time_x > t)
-    ind = inds[0][0]
-    time_x = time_x[ind:] # go from ind to final index
-    
-    data.streams[GCAMP].data = np.asarray(data.streams[GCAMP].data[ind:])
-    data.streams[ISOS].data = np.asarray(data.streams[ISOS].data[ind:])
+        decimated_GCAMP = []
+        decimated_ISOS = []
+                
+        for i in range(0, len(self.photometry_data.streams[GCAMP].photometry_data), N):        
+            # This is the moving window mean
+            decimated_GCAMP.append(np.mean(np.asarray(photometry_data.streams[GCAMP].photometry_data[i:i+N-1])))
+        photometry_data.streams[GCAMP].photometry_data = np.asarray(decimated_GCAMP)
         
-    return data
-  
+        for i in range(0, len(self.photometry_data.streams[ISOS].photometry_data), N):        
+            # This is the moving window mean
+            decimated_ISOS.append(np.mean(np.asarray(photometry_data.streams[ISOS].photometry_data[i:i+N-1])))
+        photometry_data.streams[ISOS].photometry_data = np.asarray(decimated_ISOS)
+
+        time_x = np.linspace(1,len(photometry_data.streams[GCAMP].photometry_data),
+        len(photometry_data.streams[GCAMP].photometry_data))/photometry_data.streams[GCAMP].fs
+        # time_x = resample(photometry_data)
+        # time_x = time_x[::N] # go from beginning to end of array in steps on N
+        # time_x = time_x[:len(photometry_data.streams[GCAMP].photometry_data)]
+            
+        return photometry_data
+
+    def artifact_removal(photometry_data):
+        """There is often a large artifact on the onset
+        of LEDs turning on. This function removes photometry_data below a set time t
+        
+        Parameters:
+        -----------
+        photometry_data : tdt block
+        channel : global variable
+            either GCAMP or ISOS
+        """
+        
+        
+        t = 8
+        time_x = resample(photometry_data)
+        
+        inds = np.where(time_x > t)
+        ind = inds[0][0]
+        time_x = time_x[ind:] # go from ind to final index
+        
+        photometry_data.streams[GCAMP].photometry_data = np.asarray(photometry_data.streams[GCAMP].photometry_data[ind:])
+        photometry_data.streams[ISOS].photometry_data = np.asarray(photometry_data.streams[ISOS].photometry_data[ind:])
+            
+        return photometry_data
     
-def detrending(data, time=False):
-    """Full trace dFF according to Lerner et al. 2015
-       dFF using 405 fit as baseline
+        
+    def detrending(photometry_data, time=False):
+        """Full trace dFF according to Lerner et al. 2015
+        dFF using 405 fit as baseline
+        """
+        x = np.array(photometry_data.streams[ISOS].photometry_data)
+        y = np.array(photometry_data.streams[GCAMP].photometry_data)
+        bls = np.polyfit(x, y, 1)
+        Y_fit_all = np.multiply(bls[0], x) + bls[1]
+        Y_dF_all = y - Y_fit_all
+
+        dFF = np.multiply(100, np.divide(Y_dF_all, Y_fit_all))
+        std_dFF = np.std(dFF)
+
+        npts = len(dFF)
+        time_x = np.linspace(1, npts, npts) / photometry_data.streams[GCAMP].fs
+        
+        if time:
+            return dFF, time_x
+        else:
+            return dFF
+    # TODO: add a function to calculate dFF based on this.
     """
-    x = np.array(data.streams[ISOS].data)
-    y = np.array(data.streams[GCAMP].data)
-    bls = np.polyfit(x, y, 1)
-    Y_fit_all = np.multiply(bls[0], x) + bls[1]
-    Y_dF_all = y - Y_fit_all
+    YP's matlab code for dfof calculation
+    x1=photData(1).data;
+    x2=photData(2).data;
+    reg = polyfit(x2,x1,1);
+    disp(reg)
+    if reg(1)<0
+        f0=mean(x1);
+    else
+        f0=reg(1).*x2+reg(2);
+    end
+    delF=100.*(x1-f0)./f0;
+    
+    x1 and x2 are the two channels of photometry_data (465 and 405)
+  """
+    
+        
+    def plotting(photometry_data, ax=None, kind='raw'):
+        """ plotting the fiberphotometry traces
+        
+        Parameters:
+        -----------
+        photometry_data: tdt photometry_data
+        kind: string
+            'raw' - raw photometry_data as it was recorded
+            'rawDemod' - raw demodulated photometry_data, with artifact removal
+            'dfof' - delta F over F plot
+        ax : axes object (optional)
+        N : int
+            number of points for averaging 
+        
+        """
+        
+        if ax is None:
+            ax=plt.gca()
+                
+        if (kind=='raw'):
+            time_x = resample(photometry_data)
+            p1, = ax.plot(time_x, np.asarray(photometry_data.streams[GCAMP].photometry_data),
+            linewidth=.2, color='green', label='GCaMP')
+            p2, = ax.plot(time_x, np.asarray(photometry_data.streams[ISOS].photometry_data),
+            linewidth=.2, color='blueviolet', label='ISOS')
+            ax.set_title('Raw Demodulated Responses')
+            ax.set_ylabel('mV')
+            ax.set_xlabel('Seconds')
+            ax.set_title('raw photometry traces')
+            ax.legend(handles=[p1,p2], loc='upper right')
+        
+        elif (kind=='rawDemod'):
+            photometry_data = artifact_removal(photometry_data)
+            time_x = resample(photometry_data)
+            p1, = ax.plot(time_x, np.asarray(photometry_data.streams[GCAMP].photometry_data),
+            linewidth=.2, color='green', label='GCaMP')
+            p2, = ax.plot(time_x, np.asarray(photometry_data.streams[ISOS].photometry_data),
+            linewidth=.2, color='blueviolet', label='ISOS')    
+            ax.set_ylabel('mV')
+            ax.set_xlabel('Seconds')
+            ax.set_title('Demodulated photometry traces')
+            ax.legend(handles=[p1,p2], loc='upper right')
+        
+        elif (kind=='dfof'):                    
+            dFF, time_x = detrending(photometry_data, time=True)        
+            p1, = ax.plot(time_x, np.array(dFF), linewidth=.2,
+                color='green', label='GCaMP')
+            ax.set_ylabel(r'$\Delta$F/F')
+            ax.set_xlabel('Seconds')
+            ax.legend(handles=[p1], loc='upper right')
+            ax.set_title('dFoF')
 
-    dFF = np.multiply(100, np.divide(Y_dF_all, Y_fit_all))
-    std_dFF = np.std(dFF)
-
-    npts = len(dFF)
-    time_x = np.linspace(1, npts, npts) / data.streams[GCAMP].fs
-    
-    if time:
-        return dFF, time_x
-    else:
-        return dFF
-    
-    
-    
-def plotting(data, ax=None, kind='raw'):
-    """ plotting the fiberphotometry traces
-    
-    Parameters:
-    -----------
-    data: tdt data
-    kind: string
-        'raw' - raw data as it was recorded
-        'rawDemod' - raw demodulated data, with artifact removal
-        'dfof' - delta F over F plot
-    ax : axes object (optional)
-    N : int
-        number of points for averaging 
-     
-    """
-    
-    if ax is None:
-        ax=plt.gca()
-             
-    if (kind=='raw'):
-        time_x = resample(data)
-        p1, = ax.plot(time_x, np.asarray(data.streams[GCAMP].data),
-         linewidth=.2, color='green', label='GCaMP')
-        p2, = ax.plot(time_x, np.asarray(data.streams[ISOS].data),
-         linewidth=.2, color='blueviolet', label='ISOS')
-        ax.set_title('Raw Demodulated Responses')
-        ax.set_ylabel('mV')
-        ax.set_xlabel('Seconds')
-        ax.set_title('raw photometry traces')
-        ax.legend(handles=[p1,p2], loc='upper right')
-     
-    elif (kind=='rawDemod'):
-        data = artifact_removal(data)
-        time_x = resample(data)
-        p1, = ax.plot(time_x, np.asarray(data.streams[GCAMP].data),
-         linewidth=.2, color='green', label='GCaMP')
-        p2, = ax.plot(time_x, np.asarray(data.streams[ISOS].data),
-         linewidth=.2, color='blueviolet', label='ISOS')    
-        ax.set_ylabel('mV')
-        ax.set_xlabel('Seconds')
-        ax.set_title('Demodulated photometry traces')
-        ax.legend(handles=[p1,p2], loc='upper right')
-    
-    elif (kind=='dfof'):                    
-        dFF, time_x = detrending(data, time=True)        
-        p1, = ax.plot(time_x, np.array(dFF), linewidth=.2,
-               color='green', label='GCaMP')
-        ax.set_ylabel(r'$\Delta$F/F')
-        ax.set_xlabel('Seconds')
-        ax.legend(handles=[p1], loc='upper right')
-        ax.set_title('dFoF')
-
-    return ax    
+        return ax    
--- src/video.py
+++ src/video.py
@@ -4,28 +4,29 @@ by: Gergely Turi
 gt2253@cumc.columbia.edu 
 """
 from dataclasses import dataclass
-import tdt
-
+import cv2
 
 @dataclass
-class Video:
+class UsbVideo:
     """Video class to handle video files recored with USB camera."""
 
-    f_path: str
+    vid_path: str
     
     def __post_init__(self):
-        """Reads in the data from the hdf5 file."""
-        self.data = tdt.read_block(self.f_path)
+        """Reads in all the data from tdt recording. 
+        f_path: str
+            Path to the folder containing data."""
+        self.video = cv2.VideoCapture(self.vid_path)
 
-    def frame_rate(self, rounded=True) -> float or int:
-        """Calculates frame rate based on the video meta data.
-        Returns: int or float depending on the rounded parameter.
+    @property
+    def video_params(self) -> dict:
+        """Returns the parameters of the recorded video.
         """
-        recording_lenght = self.data.info["duration"].total_seconds()
-        frames = len(self.data["epocs"]["Cam1"]["data"])
-        f_rate = frames / recording_lenght
-        if rounded:
-            fr_rate = round(f_rate)
-        else:
-            fr_rate = f_rate
-        return fr_rate
+        params = {}
+        vcap = self.video
+        if vcap.isOpened(): 
+        # get vcap property 
+            params['width']  = vcap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+            params['height'] = vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`  
+            params['fps'] = vcap.get(cv2.CAP_PROP_FPS)
+        return params
