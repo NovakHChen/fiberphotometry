9c532b2ed41ec5c83a1b0a033963c1e33dcdc9ef
Gergo <gt2253@cumc.columbia.edu>
2023-09-29 02:01:10 +0000

    adding slicing function to video.py

--- src/video.py
+++ src/video.py
@@ -3,6 +3,7 @@ Routine to handle video files.
 by: Gergely Turi
 gt2253@cumc.columbia.edu 
 """
+from os.path import join
 from dataclasses import dataclass
 import cv2
 
@@ -23,10 +24,47 @@ class UsbVideo:
         """Returns the parameters of the recorded video.
         """
         params = {}
-        vcap = self.video
-        if vcap.isOpened(): 
-        # get vcap property 
-            params['width']  = vcap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
-            params['height'] = vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`  
-            params['fps'] = vcap.get(cv2.CAP_PROP_FPS)
+        cap = self.video
+        if cap.isOpened(): 
+        # get cap property 
+            params['width']  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+            params['height'] = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`  
+            params['fps'] = cap.get(cv2.CAP_PROP_FPS)
+            params['frame_count'] = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        cap.release()
         return params
+
+    def slice_video(self, start: int, end: int, codec: str) -> None:
+        """Slices the video between start and end frames.
+        Parameters:
+        -----------
+        start: int
+            Start frame.
+        end: int
+            End frame.
+        """
+        cap = self.video
+        save_path = self.vid_path.split('.')[0]
+        
+        # setup the video writer
+        w_frame, h_frame = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),\
+                            int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = cap.get(cv2.CAP_PROP_FPS)                        
+        cap.set(cv2.CAP_PROP_POS_FRAMES, start)
+        fourcc = cv2.VideoWriter_fourcc(*codec)
+        out = cv2.VideoWriter(join(save_path,'_sliced.avi'), fourcc, fps,
+                               (w_frame, h_frame))        
+        counter = 0
+        # read until the 'end'
+        while(cap.isOpened() and counter <= end):
+            ret, frame = cap.read()
+            counter += 1
+            if ret == True:
+                if counter >= start:
+                    out.write(frame)
+            else:
+                break
+        cap.release()
+        out.release()
+        return None
+
